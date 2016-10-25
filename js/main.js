ea2d2006a26903c871b62ecf70bf2ab439faa311
swimly <979741120@qq.com>
2016-10-25 16:30:09 +0800

    finish

---
+++ js/main.js
@@ -0,0 +1,20 @@
+;(function(){
+  var current=0;
+  $(".next").click(function(){
+    current++;
+    $(".item").eq(current).show().siblings(".item").hide();
+    $(".step").children().removeClass("next")
+  })
+  $(".pop-xieyi").click(function(){
+    $(".mask").fadeIn(200);
+    setTimeout(function(){
+      $(".pop").css("top",0)
+    },500)
+  })
+  $(".close").click(function(){
+    $(this).parents(".pop").css("top","-100%");
+    setTimeout(function(){
+      $(".mask").fadeOut(200)
+    },500)
+  })
+})()
